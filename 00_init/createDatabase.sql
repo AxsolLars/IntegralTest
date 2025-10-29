87d3eeaa749a18c846141b835d5b12c801fea978
Lars <ltoensing@axsol.eu>
2025-10-29 09:53:39 +0100

    Adds the createDatabase.sql script

---
+++ 00_init/createDatabase.sql
@@ -0,0 +1,10 @@
+USE ROLE SYSADMIN;
+USE WAREHOUSE compute_wh;
+
+CREATE OR REPLACE DATABASE testing_db COMMENT = 'Testing sandbox database';
+CREATE OR REPLACE SCHEMA integral_test COMMENT = 'Schema for integration and test experiments';
+
+USE DATABASE testing_db;
+USE SCHEMA integral_test;
+
+SELECT CURRENT_ROLE(), CURRENT_DATABASE(), CURRENT_SCHEMA();
